6915b47154c9188123f01a02f5389094e7db06a2
Patrick <privat@skyraptor.eu>
2022-05-30 01:13:18 +0200

    Update interaction.ts

--- src/api/controllers/interaction.ts
+++ src/api/controllers/interaction.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
 export class InteractionController {
+  commandsCallback(req: Request, res: Response, next: NextFunction) {
+    res.sendStatus(204);
+  }
+  
   interactionCallback(req: Request, res: Response, next: NextFunction) {
     res.sendStatus(204);
   }
