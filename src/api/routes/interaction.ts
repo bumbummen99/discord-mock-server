b3971fcc6165c39cf4ad2ce3d099150de854830a
Patrick <privat@skyraptor.eu>
2022-05-30 01:13:53 +0200

    Update interaction.ts

--- src/api/routes/interaction.ts
+++ src/api/routes/interaction.ts
@@ -5,6 +5,11 @@ const interactionRouter = express.Router();
 const interactionController = new InteractionController();
 
 interactionRouter.post(
+  '/applications/:applicationId/guilds/:guildId/commands',
+  interactionController.commandsCallback
+)
+
+interactionRouter.post(
   '/interactions/:interactionId/:interactionToken/callback',
   interactionController.interactionCallback
 );
